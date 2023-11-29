3df27f5e881870d9cd8d7f574533ee15fe5505b1
Jingfeng Yang <jingfengyang@Jingfengs-MacBook-Air.local>
2023-11-29 11:52:58 -0800

    integrate historic scraper into our tool

--- gsv-bias-scraper/setup.py
+++ gsv-bias-scraper/setup.py
@@ -7,6 +7,7 @@ setup(
         'console_scripts': [
             'gsv_metadata_scrape = gsv_metadata_scraper:main',
             'gsv_visualize = gsv_visualizer:main',
+            'gsv_historic_dates_scrape = gsv_historic_dates_scraper:main',
         ]
     },
 )
