9403014ffc40966c4d7556022a0a3fc33371cf54
Jon Froehlich <jonf@cs.uw.edu>
2024-12-13 10:23:25 -0800

    added compare data (not tested yet)

--- gsv_metadata_tracker/__init__.py
+++ gsv_metadata_tracker/__init__.py
@@ -1,4 +1,19 @@
+# __init__.py
+
+from .download import download_gsv_metadata  # single-threaded version
+from .download_async import download_gsv_metadata_async  # async version
 from .config import load_config
-from .gsv import download_gsv_metadata_async
-from .geoutils import get_city_coordinates, get_city_bounding_box
-from .vis import create_visualization_map
+from .geocoding import get_city_coordinates, get_city_bounding_box
+from .visualization import create_visualization_map, display_search_area
+from .comparator import compare_gsv_files  # new comparison utility
+
+__all__ = [
+    'download_gsv_metadata',        # single-threaded download
+    'download_gsv_metadata_async',  # async download
+    'load_config',                  # configuration loader
+    'get_city_coordinates',         # geocoding utilities
+    'get_city_bounding_box',
+    'create_visualization_map',     # visualization
+    'display_search_area',
+    'compare_gsv_files',           # comparison utility
+]
